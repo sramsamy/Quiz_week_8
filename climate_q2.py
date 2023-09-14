537b560e933c50a74f95b6c947bbd7be218f54d3
Sachin Ramsamy <sachin_drz125@hotmail.com>
2023-09-14 14:55:47 +1000

    Loaded the data from the climate.csv file using pandas and then appended each columns value to the corresponding list using iterrows, showing them in a figure

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+df = pd.read_csv(r'climate.csv')
 
 years = []
 co2 = []
 temp = []
+
+# Iterate through each row of the dataframe and append values to the lists
+for index, row in df.iterrows():
+    years.append(row['Year'])
+    co2.append(row['CO2'])
+    temp.append(row['Temperature'])
+
+# testing to make sure I've appended the correct values to each list
+print(years, co2, temp)
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
